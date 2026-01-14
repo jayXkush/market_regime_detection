7782a3326d3e7bfe647f117acb9cfec7703942e6
Jay Kushwaha <jaykushwaha1651211@gmail.com>
2026-01-14 22:09:41 +0530

    Fix import: add JSONResponse

--- api.py
+++ api.py
@@ -12,6 +12,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 
 
